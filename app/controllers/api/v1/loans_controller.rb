19efb086af976c1fe64a2bb39ead9262631991f1
yammik <mkim0514@gmail.com>
2019-01-09 07:55:25 -0500

    update routing

--- app/controllers/api/v1/loans_controller.rb
+++ app/controllers/api/v1/loans_controller.rb
@@ -1,8 +1,4 @@
 class Api::V1::LoansController < ApplicationController
-  def index
-    render json: { loans: Loan.all }
-  end
-
   def show
     render json: { loan: Loan.find(params[:id]) }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :loans, only: %i(index show)
+      resources :loans, only: %i(show)
       resources :states, only: %i(index show)
       resources :purposes, only: %i(index show)
       resources :grades, only: %i(index show)
