27835a2aeee02ea7e24142147ac4e368e8d24be0
yammik <mkim0514@gmail.com>
2019-01-09 06:47:51 -0500

    update readme

--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 Parses the CSV file available for download from the Lending Club website.
 
 ### Usage
-
+Two ways.
+One: clone this repo and :
+1. `rails db:create && rails db:migrate && rails db:seed`
+2.
+Two: use the API I deployed on Heroku.
+# URL :
 
 ### Tables :
 ##### purpose
@@ -13,9 +18,4 @@ Lending Club assigns a letter grade for the application based on several paramet
 ##### addr_state
 The state which the address on the loan application belongs to.
 
-a loan belongs to purpose; purpose has many loans
-a loan belongs to grade; grade has many loans
-a loan belongs to addr_state; addr_state has many loans
-
-
-desc content begins after the '> '
+Since this is a fairly large dataset, I created tables of attributes I'm interested in looking into, and created relationships. There are a bunch I omitted for the sake of time, but would have liked to check out other relationships as well. 
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,19 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2019_01_08_061521) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "grades", force: :cascade do |t|
     t.string "letter"
@@ -8,9 +23,9 @@ ActiveRecord::Schema.define(version: 2019_01_08_061521) do
 
   create_table "loans", force: :cascade do |t|
     t.integer "loan_amnt"
-    t.integer "grade_id"
-    t.integer "purpose_id"
-    t.integer "state_id"
+    t.bigint "grade_id"
+    t.bigint "purpose_id"
+    t.bigint "state_id"
     t.string "home_owner"
     t.integer "annual_inc"
     t.string "issue_d"
@@ -39,4 +54,7 @@ ActiveRecord::Schema.define(version: 2019_01_08_061521) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "loans", "grades"
+  add_foreign_key "loans", "purposes"
+  add_foreign_key "loans", "states"
 end
